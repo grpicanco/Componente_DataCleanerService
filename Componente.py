96252df1a19cf5ca37a298b5f56fe0aa8efe0850
Gabriel Rodrigues <gabee_picanco@hotmail.com>
2023-06-04 21:30:44 -0400

    Adição do  componente principal do componente

--- Componente.py
+++ Componente.py
@@ -1,8 +1,11 @@
+import json
+
 import requests
 
 
 class Regra:
-    def __init__(self, ulr, data_criacao, data_modificacao, codigo, titulo, descricao, tipo, script):
+    def __init__(self, ulr=None, data_criacao=None, data_modificacao=None, codigo=None, titulo=None, descricao=None,
+                 tipo=None, script=None):
         self._url = ulr
         self._data_criacao = data_criacao
         self._data_modificacao = data_modificacao
@@ -12,6 +15,58 @@ class Regra:
         self._tipo = tipo
         self._script = script
 
+    @property
+    def url(self):
+        return self._url
+
+    @url.setter
+    def url(self, value):
+        self._url = value
+
+    def to_json(self):
+        data = {
+            'codigo': self._codigo,
+            'titulo': self._titulo,
+            'descricao': self._descricao,
+            'tipo': self._tipo,
+            'script': self._script
+        }
+        return json.dumps(data)
+
+    def __str__(self):
+        return f'{self._codigo}: {self._titulo}'
+
+
+class AcaoDeCorrecao():
+    def __init__(self, ulr=None, data_criacao=None, data_modificacao=None, codigo=None, titulo=None, descricao=None,
+                 tipo=None, script=None):
+        self._url = ulr
+        self._data_criacao = data_criacao
+        self._data_modificacao = data_modificacao
+        self._codigo = codigo
+        self._titulo = titulo
+        self._descricao = descricao
+        self._tipo = tipo
+        self._script = script
+
+    @property
+    def url(self):
+        return self._url
+
+    @url.setter
+    def url(self, value):
+        self._url = value
+
+    def to_json(self):
+        data = {
+            'codigo': self._codigo,
+            'titulo': self._titulo,
+            'descricao': self._descricao,
+            'tipo': self._tipo,
+            'script': self._script
+        }
+        return json.dumps(data)
+
     def __str__(self):
         return f'{self._codigo}: {self._titulo}'
 
@@ -19,8 +74,15 @@ class Regra:
 class Componente:
     def __init__(self):
         self._regras = {}
+        self._acao_de_correcao = {}
 
     def listar_regras(self):
+        """
+        Lista todas as regras disponíveis.
+
+        :return: Lista de regras.
+        :raises Exception: Erro na requisição.
+        """
         response = requests.get('http://localhost:8000/regras/')
         if response.status_code == 200:
             data = response.json()
@@ -43,14 +105,100 @@ class Componente:
             raise Exception(f'Erro na requisição: {response.status_code}')
 
     def detalhar_regra(self, codigo: str):
-        regra = self._regras.get(codigo)
-        if regra:
-            return regra
+        """
+        Detalha uma regra específica pelo código.
+
+        :param codigo: Código da regra a ser detalhada.
+        :return: Detalhes da regra.
+        :raises Exception: Regra não encontrada.
+        """
+        if self._regras:
+            regra = self._regras.get(codigo)
+            if regra:
+                return regra
+            else:
+                raise Exception(f'Regra com código {codigo} não encontrada.')
         else:
-            raise Exception(f'Regra com código {codigo} não encontrada.')
+            self.listar_regras()
+            return self.detalhar_regra(codigo)
 
-    def criar_regra(self):
-        pass
+    def listar_acao_de_correcao(self):
+        """
+        Lista todas as ações de correção disponíveis.
 
-    def excluir_regra(self):
-        pass
+        :return: Lista de ações de correção.
+        :raises Exception: Erro na requisição.
+        """
+        response = requests.get('http://localhost:8000/acaodecorrecao/')
+        if response.status_code == 200:
+            data = response.json()
+            self._acao_de_correcao = {}
+
+            for item in data:
+                url = item.get('url')
+                data_criacao = item.get('data_criacao')
+                data_modificacao = item.get('data_modificacao')
+                codigo = item.get('codigo')
+                titulo = item.get('titulo')
+                descricao = item.get('descricao')
+                tipo = item.get('tipo')
+                script = item.get('script')
+
+                acao_de_correcao = AcaoDeCorrecao(url, data_criacao, data_modificacao, codigo, titulo, descricao, tipo,
+                                                  script)
+                self._acao_de_correcao[codigo] = acao_de_correcao
+            return list(self._acao_de_correcao.values())
+        else:
+            raise Exception(f'Erro na requisição: {response.status_code}')
+
+    def detalhar_acao_de_correcao(self, codigo):
+        """
+        Detalha uma ação de correção específica pelo código.
+
+        :param codigo: Código da ação de correção a ser detalhada.
+        :return: Detalhes da ação de correção.
+        :raises Exception: Ação de correção não encontrada.
+        """
+        if self._acao_de_correcao:
+            acao_de_correcao = self._acao_de_correcao.get(codigo)
+            if acao_de_correcao:
+                return acao_de_correcao
+            else:
+                raise Exception(f'Regra com código {codigo} não encontrada.')
+        else:
+            self.listar_acao_de_correcao()
+            return self.detalhar_acao_de_correcao(codigo)
+
+    def corrigir_dados(self, dados, cod_regras, cod_acao_de_correcao):
+        """
+        Corrige os dados com base nas regras e ações de correção fornecidas.
+
+        :param dados: Dados a serem corrigidos.
+        :param cod_regras: Lista de códigos das regras a serem aplicadas.
+        :param cod_acao_de_correcao: Lista de códigos das ações de correção a serem aplicadas.
+        :return: Resposta da requisição.
+        :raises Exception: Erro na requisição.
+                """
+        regras = []
+        acaos = []
+        url = 'http://localhost:8000/correcaodedados/'
+        headers = {'Content-Type': 'application/json'}
+        for item in cod_regras:
+            regra = self.detalhar_regra(item)
+            regras.append(regra.url)
+
+        for item in cod_acao_de_correcao:
+            acao = self.detalhar_acao_de_correcao(item)
+            acaos.append(acao.url)
+
+        data = {
+            'regras': regras,
+            'acao_correcao': acaos,
+            'dados': dados
+        }
+        data = json.dumps(data, separators=(",", ":"))
+        response = requests.post(url, data=data, headers=headers)
+        if response.status_code == 200:
+            return response
+        else:
+            raise f'Erro na requisição: {response.status_code}'
--- main.py
+++ main.py
@@ -1,20 +1,101 @@
 # This is a sample Python script.
+import os
+import pandas as pd
+import subprocess
 from Componente import *
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+componente = Componente()
 
 
-def print_hi():
-    regra = Componente()
-    for item in regra.listar_regras():
-        print(item)
+def ler_tabela(path):
+    """
+    Lê um arquivo de tabela (CSV, Excel, ODT ou ODS) e retorna um DataFrame do pandas.
 
-    print(regra.detalhar_regra('RG002'))
-    print(regra.detalhar_regra('RG015'))
+    :param path: Caminho do arquivo.
+    :return: DataFrame do pandas contendo os dados da tabela.
+    :raises ValueError: Formato de arquivo inválido. Apenas arquivos CSV, Excel, ODT e ODS são suportados.
+    :raises FileNotFoundError: Arquivo não encontrado.
+    :raises Exception: Erro ao ler o arquivo.
+    """
+    try:
+        if path.endswith('.csv'):
+            # Ler arquivo CSV
+            df = pd.read_csv(path, delimiter=',')
+        elif path.endswith(('.xls', '.xlsx')):
+            # Ler arquivo Excel
+            df = pd.read_excel(path)
+        elif path.endswith('.odt') or path.endswith('.ods'):
+            # Obter o nome do arquivo sem a extensão
+            nome_arquivo, extensao = os.path.splitext(path)
+
+            # Converter para CSV usando o LibreOffice
+            csv_path = nome_arquivo + '.csv'
+            subprocess.call(['soffice', '--headless', '--convert-to', 'csv', '--outdir', '.', path])
+
+            # Ler arquivo CSV convertido usando o pandas
+            df = pd.read_csv(csv_path, delimiter=',')
+
+            # Remover o arquivo CSV convertido
+            subprocess.call(['rm', csv_path])
+        else:
+            raise ValueError('Formato de arquivo inválido. Apenas arquivos CSV, Excel, ODT e ODS são suportados.')
+
+        return df
+    except FileNotFoundError:
+        print(f"Arquivo não encontrado: {path}")
+    except Exception as e:
+        print(f"Erro ao ler o arquivo: {e}")
+
+    return None
+
+
+def obter_lista_colunas(dataframe) -> list:
+    """
+    Transforma as colunas do DataFrame em linhas e retorna uma lista dos valores por coluna.
+
+    :param dataframe: DataFrame do pandas.
+    :return: Lista de valores por coluna do DataFrame.
+    """
+    df = dataframe.fillna('-').transpose().values.tolist()
+    return df
+
+
+def adicionar_acao_de_correcao(dado):
+    pass
+
+
+def main():
+    resp = 0
+    path = '~//Documentos//Untitled_1.csv'
+    df = ler_tabela(path)
+    regras = {
+        0: ["RG001"],
+        1: ['RG006'],
+        2: [],
+        3: [],
+        4: [],
+        5: [],
+    }
+    acao = {
+        0: ['AC001', 'AC002'],
+        1: [],
+        2: [],
+        3: [],
+        4: [],
+        5: []
+    }
+    corrigido = []
+    lista_dados = obter_lista_colunas(df)
+    item = 0
+    for dado in lista_dados:
+        dado = componente.corrigir_dados(dados=dado, cod_regras=regras.get(item), cod_acao_de_correcao=acao.get(item))
+        corrigido.append(dado.json())
+        item += 1
+    print(corrigido)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi()
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
